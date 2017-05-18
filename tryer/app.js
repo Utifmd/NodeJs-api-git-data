16e9e66bfb5283a96fb6bad9722ce73175c01f33
UTIF Milkedori <utif@localhost.localdomain>
2017-05-18 17:15:06 +0700

    just addintional

---
+++ tryer/app.js
@@ -0,0 +1,11 @@
+/* let profiles = (tampil) => {
+     console.log(`tampil dengan log ini ${tampil}`)
+ }
+ profiles('muantabs')*/
+
+ let binatangs = ['ayam', 'sapi', 'kera', 'ayam']
+
+ let result = binatangs.map(ekor => {
+     return ekor + ' ntap soul'
+ })
+ console.log(result)
